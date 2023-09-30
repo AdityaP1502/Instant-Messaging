cd7e83e13994f1c4360f71f4ff63d7aa95045082
AdityaP1502 <i.m.aditya015@gmail.com>
2023-09-30 20:21:13 +0700

    Add unit test for hmac

---
+++ ForwarderServer/src/test/java/com/ForwarderServer/server/util/HmacTest.java
@@ -0,0 +1,54 @@
+package com.ForwarderServer.server.util;
+
+import org.junit.jupiter.api.Test;
+
+import java.security.InvalidKeyException;
+
+import org.junit.jupiter.api.Assertions;
+
+import com.ForwarderServer.server.util.CryptoHandler;
+
+public class HmacTest {
+	@Test
+	public static void hexToBytesTest()
+	{
+		byte[] res = CryptoHandler.hexStringToByteArray("FFAAAF0F1F2AB3");
+		byte[] expected = {-1, -86, -81, 15, 31, 42, -77};
+		Assertions.assertArrayEquals(expected, res);
+	}
+	
+	@Test
+	public static void bytesToHexTest()
+	{
+		byte[] arg = {-1, -86, -81, 15, 31, 42, -77};
+		String hex = CryptoHandler.bytesToHexString(arg);
+		String expected = "FFAAAF0F1F2AB3";
+		Assertions.assertEquals(expected.toLowerCase(), hex);
+	}
+	
+	@Test
+	public static void hmacTest()
+	{
+		String sessionKey = "Aditya1502";
+		String key = "FF0A12";
+		
+		try {
+			CryptoHandler.getAccessToken(sessionKey, key);
+		} catch (InvalidKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("Failed");
+			System.exit(1);
+		}
+	}
+	
+	public static void main(String[] args)
+	{
+		hexToBytesTest();
+		System.out.println("Passed");
+		bytesToHexTest();	
+		System.out.println("Passed");
+		hmacTest();
+		System.out.println("Passed");
+	}
+}
