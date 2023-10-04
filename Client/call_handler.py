0318e117140ee9ba4560826e8d25691138da984b
AdityaP1502 <i.m.aditya015@gmail.com>
2023-10-04 21:46:47 +0700

    Implement call handler again

---
+++ Client/call_handler.py
@@ -0,0 +1,41 @@
+import os
+import os.path
+import sys
+
+from process.process_handler import ProcessHandler
+
+class CallHandler():
+    def __init__(self) -> None:
+        self._p_handler = None
+    
+    def spawn_process(self, restype, sender, recipient, token, salt, network_address):
+        shortopts = [
+                ["-s", sender], 
+                ["-r", recipient], 
+            ]
+            
+        longopts = [
+            ["--token", token], 
+            ["--salt", salt], 
+            ["--network", network_address], 
+        ]
+            
+        longopts.append(["--caller", ""] if restype == "CHANNEL_ALLOCATION" else ["--receiver", ""])
+            
+        path = os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), "process/call/main.py")
+        self._p_handler = ProcessHandler(cmd="python {}".format(path), shortopts=shortopts, longopts=longopts)
+        self._p_handler.run()
+    
+    def check_process_status(self):
+        if self._p_handler == None:
+            return False
+        
+        return self._p_handler.is_spawned_process_alive()
+    
+    def force_stop(self):
+        self._p_handler.force_terminate()
+        self._p_handler.join()
+        
+    
+    
+    
