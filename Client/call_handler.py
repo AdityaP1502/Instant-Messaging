b2980eadbbb3d7b5b83a6f509dd9b80f06a2e863
AdityaP1502 <i.m.aditya015@gmail.com>
2023-12-14 05:01:58 +0700

    fix wrong sender/recipient in receiver

--- Client/call_handler.py
+++ Client/call_handler.py
@@ -9,6 +9,9 @@ class CallHandler():
         self._p_handler = None
     
     def spawn_process(self, restype, sender, recipient, token, salt, network_address):
+        if restype != "CHANNEL_ALLOCATION":
+            sender, recipient = recipient, sender
+            
         shortopts = [
                 ["-s", sender], 
                 ["-r", recipient], 
@@ -22,7 +25,11 @@ class CallHandler():
             
         longopts.append(["--caller", ""] if restype == "CHANNEL_ALLOCATION" else ["--receiver", ""])
             
-        path = os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), "process/call/main.py")
+        path = os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), "process", "call", "main.py")
+        
+        # Use this handler when trying to debug, the terminal will stay open when an error occured  
+        # self._p_handler = ProcessHandler(cmd="cmd /K python {}".format(path), shortopts=shortopts, longopts=longopts)
+        
         self._p_handler = ProcessHandler(cmd="python {}".format(path), shortopts=shortopts, longopts=longopts)
         self._p_handler.run()
     
