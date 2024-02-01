5d8a0c858a6e3720f90fc357fb64e75b9f66c479
AdityaP1502 <i.m.aditya015@gmail.com>
2024-02-01 23:36:32 +0700

    Add unit test

---
+++ api/model/model_test.go
@@ -0,0 +1,48 @@
+package model
+
+import (
+	"testing"
+)
+
+func TestFieldToQueryOnce(t *testing.T) {
+	keys := []string{"username", "name", "password", "is_active"}
+
+	query := transformNamesToUpdateQuery(keys, 1)
+	expected := "username=$1,name=$2,password=$3,is_active=$4"
+
+	if query != expected {
+		t.Errorf("Expected %s received %s", expected, query)
+	}
+}
+
+func TestFieldToQueryTwice(t *testing.T) {
+	keys := []string{"username", "name", "password", "is_active"}
+	condition := []string{"foo", "bar"}
+
+	_ = transformNamesToUpdateQuery(keys, 1)
+	query2 := transformNamesToUpdateQuery(condition, len(keys)+1)
+
+	if query2 != "foo=$5,bar=$6" {
+		t.Errorf("Expected foo=$5,bar=$6 received %s", query2)
+	}
+}
+
+func TestGetNonEmptyField(t *testing.T) {
+	data := &Account{
+		Username: "asep",
+		Password: "1234",
+	}
+
+	keys, _ := getNonEmptyField(data)
+
+	t.Log(keys)
+
+	query := transformNamesToUpdateQuery(keys, 1)
+
+	expected := "username=$1,password=$2"
+
+	if query != expected {
+		t.Errorf("Expected %s received %s", expected, query)
+	}
+
+}
