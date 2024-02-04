e36ad305097130cde37e64e507db3668fab8f8e1
AdityaP1502 <i.m.aditya015@gmail.com>
2024-02-04 18:17:13 +0700

    update model test to test transform with separator

--- api/model/model_test.go
+++ api/model/model_test.go
@@ -7,24 +7,28 @@ import (
 func TestFieldToQueryOnce(t *testing.T) {
 	keys := []string{"username", "name", "password", "is_active"}
 
-	query := transformNamesToUpdateQuery(keys, 1)
+	query := transformNamesToUpdateQuery(keys, 1, ",")
 	expected := "username=$1,name=$2,password=$3,is_active=$4"
 
 	if query != expected {
 		t.Errorf("Expected %s received %s", expected, query)
 	}
+
+	t.Logf("%s is correct", query)
 }
 
 func TestFieldToQueryTwice(t *testing.T) {
 	keys := []string{"username", "name", "password", "is_active"}
 	condition := []string{"foo", "bar"}
 
-	_ = transformNamesToUpdateQuery(keys, 1)
-	query2 := transformNamesToUpdateQuery(condition, len(keys)+1)
+	_ = transformNamesToUpdateQuery(keys, 1, ",")
+	query2 := transformNamesToUpdateQuery(condition, len(keys)+1, " AND ")
 
-	if query2 != "foo=$5,bar=$6" {
-		t.Errorf("Expected foo=$5,bar=$6 received %s", query2)
+	if query2 != "foo=$5 AND bar=$6" {
+		t.Errorf("Expected foo=$5 AND bar=$6 received %s", query2)
 	}
+
+	t.Logf("%s is correct", query2)
 }
 
 func TestGetNonEmptyField(t *testing.T) {
@@ -37,12 +41,14 @@ func TestGetNonEmptyField(t *testing.T) {
 
 	t.Log(keys)
 
-	query := transformNamesToUpdateQuery(keys, 1)
+	query := transformNamesToUpdateQuery(keys, 1, ",")
 
 	expected := "username=$1,password=$2"
 
 	if query != expected {
 		t.Errorf("Expected %s received %s", expected, query)
 	}
+
+	t.Logf("%s is correct", query)
 
 }
