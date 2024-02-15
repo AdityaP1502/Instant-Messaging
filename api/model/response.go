39dfda74f1833946a94c81408a7f01b78e395e94
AdityaP1502 <i.m.aditya015@gmail.com>
2024-02-15 14:05:45 +0700

    change base_response from route package to response in model package

---
+++ api/model/response.go
@@ -0,0 +1,12 @@
+package model
+
+type ErrorResponse struct {
+	Status      string `json:"status"`
+	ErrorType   string `json:"error_type"`
+	Description string `json:"description"`
+}
+
+type GenericResponse struct {
+	Status  string `json:"status"`
+	Message string `json:"message"`
+}
