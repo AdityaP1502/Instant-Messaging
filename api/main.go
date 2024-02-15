7d1b6d28b484b9ad628dfb5e7444708340c595f7
AdityaP1502 <i.m.aditya015@gmail.com>
2024-02-15 14:17:18 +0700

    add db connection

--- api/main.go
+++ api/main.go
@@ -7,8 +7,11 @@ import (
 	"strings"
 	"sync"
 
+	"github.com/AdityaP1502/Instant-Messaging/api/api/routes"
 	"github.com/AdityaP1502/Instant-Messaging/api/api/util"
 	"github.com/gorilla/mux"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
 )
 
 func main() {
@@ -19,7 +22,39 @@ func main() {
 		log.Fatal(err)
 	}
 
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		config.Database.Host, config.Database.Port, config.Database.Username, config.Database.Password, config.Database.Database)
+
+	db, err := sqlx.Open("postgres", psqlInfo)
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
 	r := mux.NewRouter()
+
+	routes.SetAccountRoute(r.PathPrefix("/v1").Subrouter(), db.DB, config)
+	// // r.Handle("/", r)
+
+	r.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("WTF is happening")
+		w.Write([]byte("Hello, world!"))
+		w.WriteHeader(200)
+	}).Methods("GET")
+
+	// a := s.PathPrefix("/account").Subrouter()
+
+	// a.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	w.Write([]byte("Hello, world!"))
+	// }).Methods("GET")
+
+	http.Handle("/", r)
 
 	// wait until the server has ended
 	var wg sync.WaitGroup
@@ -29,19 +64,24 @@ func main() {
 		defer wg.Done()
 		if strings.ToLower(config.Server.Secure) == "true" {
 			http.ListenAndServeTLS(
-				fmt.Sprintf("%s:%s", config.Server.Host, config.Server.Port),
+				fmt.Sprintf("%s:%d", config.Server.Host, config.Server.Port),
 				config.Certificate.CertFile,
 				config.Certificate.KeyFile,
 				r,
 			)
 		} else {
-			http.ListenAndServe(
-				fmt.Sprintf("%s:%s", config.Server.Host, config.Server.Port),
+			err := http.ListenAndServe(
+				fmt.Sprintf("%s:%d", config.Server.Host, config.Server.Port),
 				r,
 			)
+
+			if err != nil {
+				panic(err)
+			}
 		}
 	}()
 
-	fmt.Printf("Server is running on %s:%s\n", config.Server.Host, config.Server.Port)
+	fmt.Printf("Server is running on %s:%d\n", config.Server.Host, config.Server.Port)
 	wg.Wait()
+
 }
