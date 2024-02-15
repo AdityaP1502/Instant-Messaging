c9c085376f512bbb318b87c2065b7e13468932e7
AdityaP1502 <i.m.aditya015@gmail.com>
2024-02-15 14:18:13 +0700

    update database field

--- api/scripts/make_table.sh
+++ api/scripts/make_table.sh
@@ -1,23 +1,25 @@
 CREATE TABLE account(
 	account_id SERIAL PRIMARY KEY,
-	username CHAR(64) NOT NULL
-	name CHAR(64) NOT NULL,
-	password CHAR(64) NOT NULL,
+	username VARCHAR(64) NOT NULL,
+	name VARCHAR(64) NOT NULL,
+	password vARCHAR(80) NOT NULL,
+    password_salt VARCHAR(64) NOT NULL,
 	email CHAR(64) NOT NULL,
-	is_active bool NOT NULL
+	is_active VARCHAR(6) NOT NULL
 );
 
 CREATE TABLE invalid_token(
 	token_id SERIAL PRIMARY KEY,
-	account_id INT references account(account_id),
+	username VARCHAR(64) references account(username) ON DELETE CASCADE,
 	token VARCHAR(1024) NOT NULL,
 	type CHAR(16) NOT NULL,
 	expired_at TIMESTAMP NOT NULL
 );
 
 CREATE TABLE user_otp(
-	account_id INT references account(account_id),
-	otp CHAR(16) NOT NULL,
+	username  VARCHAR(64) references account(username) ON DELETE CASCADE,
+    otp_confirmation_id varchar(512) NOT NULL, 
+	otp CHAR(6) NOT NULL,
 	last_resend TIMESTAMP NOT NULL,
-	marked_for_deletion bool NOT NULL
+	marked_for_deletion VARCHAR(6) NOT NULL
 );
