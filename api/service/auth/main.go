d2bac2b05dddc797473339b5aa3647466712f3da
AdityaP1502 <i.m.aditya015@gmail.com>
2024-02-28 18:07:17 +0700

    update server tls configuration

--- api/service/auth/main.go
+++ api/service/auth/main.go
@@ -1,18 +1,28 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 	"sync"
 	"time"
 
+	"github.com/AdityaP1502/Instant-Messanging/api/http/httputil"
 	"github.com/AdityaP1502/Instant-Messanging/api/service/auth/config"
 	"github.com/AdityaP1502/Instant-Messanging/api/service/auth/routes"
 	"github.com/gorilla/mux"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
+)
+
+var (
+	PASSPHRASE       = "PASSPHRASE"
+	CERT_FILE_PATH   = "CERT_FILE_PATH"
+	PRIVATE_KEY_PATH = "PRIVATE_KEY_PATH"
+	ROOT_CA_CERT     = "ROOT_CA_CERT"
 )
 
 func main() {
@@ -55,12 +65,49 @@ func main() {
 	go func() {
 		defer wg.Done()
 		if strings.ToLower(config.Server.Secure) == "true" {
-			http.ListenAndServeTLS(
-				fmt.Sprintf("%s:%d", config.Server.Host, config.Server.Port),
-				config.Certificate.CertFile,
-				config.Certificate.KeyFile,
-				r,
+			passphrasePath := os.Getenv(PASSPHRASE)
+
+			if passphrasePath == "" {
+				passphrasePath = "/tmp/passphrase"
+			}
+
+			cert, pKey := httputil.LoadCertificate(
+				os.Getenv(CERT_FILE_PATH),
+				os.Getenv(PRIVATE_KEY_PATH),
+				passphrasePath,
 			)
+
+			rootCAPool := httputil.LoadRootCACertPool(os.Getenv(ROOT_CA_CERT))
+
+			// Inbound tls configuration
+			tlsConfig := &tls.Config{
+				Certificates: []tls.Certificate{
+					{
+						Certificate: [][]byte{cert.Raw},
+						PrivateKey:  pKey,
+					},
+				},
+				ClientCAs:  rootCAPool,
+				MinVersion: tls.VersionTLS10,
+				ClientAuth: tls.VerifyClientCertIfGiven,
+			}
+
+			srv := http.Server{
+				Addr:      fmt.Sprintf("%s:%d", config.Server.Host, config.Server.Port),
+				Handler:   r,
+				TLSConfig: tlsConfig,
+			}
+
+			// set outbound tls configuration
+			config.Config = &tls.Config{
+				Certificates: []tls.Certificate{{
+					Certificate: [][]byte{cert.Raw},
+					PrivateKey:  pKey,
+				}},
+				RootCAs: rootCAPool,
+			}
+
+			srv.ListenAndServeTLS("", "")
 		} else {
 			err := http.ListenAndServe(
 				fmt.Sprintf("%s:%d", config.Server.Host, config.Server.Port),
