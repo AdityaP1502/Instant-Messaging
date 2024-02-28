a82aa9fc0ce9081d067239ed2cfbeaadda3d9c4e
AdityaP1502 <i.m.aditya015@gmail.com>
2024-02-28 18:13:52 +0700

    redirect passphrase to file instead of env

--- cert-manager/scripts/init_key_cert.sh
+++ cert-manager/scripts/init_key_cert.sh
@@ -11,11 +11,14 @@ export ORG_UNIT="Aditya Software Certificate Manager"
 export COMMON_NAME="cert-manager.aditya.com"
 export ISSUER="/C=${COUNTRY}/ST=${STATE}/L=${LOCALITY}/O=${ORGANIZATION}/OU=${ORG_UNIT}/CN=${COMMON_NAME}"
 # export ABSOLUTE_CERT_PATH="$(pwd)/${CERT_FILE_PATH}"
-echo $CERT_FILE_PATH
-echo $PASSPHRASE
+
+echo "$(openssl rand -base64 32)" | tr -d '\n' > /tmp/passphrase
+echo "$(cat /tmp/passphrase)"
 
 openssl genpkey -algorithm RSA -out private_key.pem -pkeyopt rsa_keygen_bits:2048
 openssl req -new -x509 -key private_key.pem -out $CERT_FILE_PATH -days 365 -subj "${ISSUER}"
-openssl pkcs8 -topk8 -inform PEM -outform PEM -in private_key.pem -out $PRIVATE_KEY_PATH -passout pass:"${PASSPHRASE}"
+openssl pkcs8 -topk8 -inform PEM -outform PEM -in private_key.pem -out $PRIVATE_KEY_PATH -passout file:/tmp/passphrase
+
+
 
 rm private_key.pem
