09f1fb17e053e12d87370bd939207bd928fa6d14
AdityaP1502 <i.m.aditya015@gmail.com>
2024-03-01 00:38:31 +0700

    change fromJSON return type into responseerror.HTTPCustomError

--- api/http/payload.go
+++ api/http/payload.go
@@ -1,8 +1,12 @@
 package httpx
 
-import "io"
+import (
+	"io"
+
+	"github.com/AdityaP1502/Instant-Messanging/api/http/responseerror"
+)
 
 type Payload interface {
-	FromJSON(r io.Reader, checkRequired bool, requiredFields []string) error
+	FromJSON(r io.Reader, checkRequired bool, requiredFields []string) responseerror.HTTPCustomError
 	ToJSON(checkRequired bool, requiredFields []string) ([]byte, error)
 }
